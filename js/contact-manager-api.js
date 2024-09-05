643f7f41fbaade80064ebb27cf18e227a5bcff1b
cosjulian <89463345+Cosjulian@users.noreply.github.com>
2024-09-05 00:10:11 -0400

    Created template for future JS functions

---
+++ js/contact-manager-api.js
@@ -0,0 +1,79 @@
+const urlBase = '';
+const extension = 'php';
+
+/*
+	This function should take in new user information
+	and ensure that the user was successfully added to the 
+	database. 
+*/
+function postNewUser()
+{
+	
+}
+
+/*
+	This function should take in a username and password
+	hash the password, then validate against the backend db.
+*/
+function getLogin()
+{
+
+}
+
+/*
+	This function should take in new contanct info
+	and a userId and add the contact to the user's 
+	contact table. It will return
+*/
+function postNewContact()
+{
+
+}
+
+/*
+	This function should take in new updated contact info,
+	a contactId, and a userId. Then update it in the user's
+	contact table.
+
+*/
+function putUpdateContact()
+{
+
+}
+
+/*
+	This function should take in a userId and return
+	a list/array of contacts with limited info (i.e. return a list of 
+	contact names + ids). This method could get the first 10/50/100/all.
+*/
+function getContactPreviewList()
+{
+
+}
+
+/*
+	This function should take in a userId and return
+	a list/array of contacts with all info. This method could get the first 10/50/100/all.
+*/
+function getContactList()
+{
+
+}
+
+/*
+	This function should take in a userId and a contactId
+	and then delete the contact in the user's db table.
+*/
+function deleteContact()
+{
+
+}
+
+/*
+	This function should take in a userId and contactId and 
+	then get all the info of that particular contact
+*/
+function getContact()
+{
+
+}
