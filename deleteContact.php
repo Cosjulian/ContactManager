7f4be9f0c92218294123586604a35cec3f66fdeb
Alex Alen <105132241+aalen24@users.noreply.github.com>
2024-09-13 13:42:57 -0400

    Add files via upload

---
+++ deleteContact.php
@@ -0,0 +1,41 @@
+<?php
+    $inData = getRequestInfo();
+	
+    $phone = $inData["phone"];
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "Group24WebServer", "Group24Server", "contact_manager");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("DELETE FROM Contacts WHERE Phone=? AND UserID=?");
+		$stmt->bind_param("ss", $phone, $userId);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+
+
+?>
